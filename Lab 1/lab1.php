106d77df40d10874b49906945e42b378f20fb32d
AsadHossan <83630454+AsadHossan@users.noreply.github.com>
2021-10-28 04:00:58 +0600

    Update lab1.php

--- Lab 1/lab1.php
+++ Lab 1/lab1.php
@@ -1,90 +1,85 @@
-
-<?php
-
-    session_start();
-
-    if(isset($_POST['submit']))
-    {
-
-        $currPass = $_POST['Cpassword'];
-        $newPass=   $_POST['Npassword'];    
-
-        $RePass=    $_POST['Rpassword'];
-
-        if($currPass!=$_SESSION['password'] || $currPass==$newPass || $newPass!=$RePass)  {
-            echo "Retype, password is incorect!";
-        }
-    }
-?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Change Password</title>
+	<title>Curriculum Vitae</title>
 </head>
-<body>
+<body style="background: rgb(200,200,200);">
+	
+	<h2 align="center" style="color: rgb(106, 90, 205);">Curriculum Vitae</h2>
+	
+	    <form method="" action="">
+	    	<fieldset>
+	    		<table >
+	    		 
+	    			</tr>
+	    			<tr>
+	    				<h3>Personal Information</h3>	    				
+	    			</tr>
 
-	<table border="1" width="100%" cellspacing="0">
-        <tr>
-            <td align="right" colspan="2">
-                <a href="home.php"><img src="logo.png" align="left" height="70px"></a>
-                Logged in as <?php echo $_SESSION['username'] ?>|
-                <a href="logout.php">Logout</a>
-            </td>
-        </tr>
-        <tr height = "500px">
-            				<td align="left" width="300px" >
-
-                            		<h3>Account</h3>
-                            		<hr size="1">
-                            		<ul>
-										<li><a href="dashboard.php">Dashboard</a></li>
-										<li><a href="viewprofile.php">View Profile</a></li>
-										<li><a href="editprofile.php">Edit Profile</a></li>
-										<li><a href="changepicture.php">Change Profile Piture</a></li>
-										<li><a href="changepassword.php">Change Password</a></li>
-										<li><a href="logout.php">Logout</a></li>
-									</ul>
-
-
-           					 </td>
-            					<td height="300px"  align="center">
-                                    <br>
-                                    <fieldset style="width: 40%" >
-                                        <legend> CHANGE PASSWORD </legend>
-                                        <table>
-                                            <tr>
-                                                <td>Current Password</td>
-                                                <td>:<input type="password" name="Cpassword" value=""></td>
-                                            </tr>
-                                            <tr>
-                                                <td>New Password</td>
-                                                <td>:<input type="password" name="Npassword" value=""></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Retype Password</td>
-                                                <td>:<input type="password" name="Rpassword" value=""></td>
-                                            </tr>
-                                            <tr>
-                                                <td colspan="2">
-                                                    <hr>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td colspan="2"> 
-                                                    <input type="submit" name="submit" value="Submit">
-                                                </td>
-                                            </tr>
-                                        </table>
-                                    </fieldset>
-                                    <br> 
-                            </td>
-        </tr>
-        <tr height = "30px">
-            <td colspan="2">
-                <center> Copyright@2017 </center>
-            </td>
-        </tr>
-    </table>
-
-</body>
-</html>
+	    			<tr>
+	    				<td width="100px">Name:</td>
+	    				<td> <input type="text" name="name" value="" placeholder=""/></td>
+	    			</tr>
+	    			<tr>
+	    				<td>Address:</td>
+	    				<td> <textarea cols="15" rows="5"></textarea> </td>
+	    			</tr>
+	    			<tr>
+	    				<td>Gender:</td>
+					<td> 
+						<input type="radio" name="gender"> Male
+						<input type="radio" name="gender"> Female
+						<input type="radio" name="gender"> Other 
+					</td>
+	    			</tr>
+	    			<tr>
+	    				<td>Marital status:</td>
+	    				<td> <input type="text" name="Marital" value="" placeholder=""/></td>
+	    			</tr>
+	    			<tr>
+	    				<td>Nationality:</td>
+	    				<td> <input type="text" name="Nationality" value="" placeholder=""/></td>
+	    			</tr>
+	    			<tr>
+	    				<td>Date Of Birth:</td>
+	    				<td> <input type="date" name="dob" value="" placeholder="dob" /></td>
+	    			</tr>
+	    			<tr>
+	    				<td>Email:</td>
+	    				<td> <input type="email" name="email" value="" placeholder="Enter Your email"/></td>
+	    			</tr>
+	    			<tr>
+	    				<td>Mobile:</td>
+	    				<td> <input type="number" name="Number" value="" placeholder="Enter valid Number"/></td>
+	    			</tr>
+	    		</table>
+	    		<table border="1">
+	    			<h3>Educational Qualification</h3>
+	    			<tr>
+	    				<td height="50px" width="500px" align="center">Name of Degree</td>
+	    				<td width="500px" align="center">Institution</td>
+	    				<td width="500px" align="center">Group</td>
+	    				<td width="500px" align="center">Passing Year</td>
+	    				<td width="500px" align="center">GPA</td>
+	    			</tr>
+	    			<tr>
+	    				<td height="50px" width="500px" align="center">SSC</td>
+	    				<td width="500px" align="center">Basurhat A.H.C government high school </td>
+	    				<td width="500px" align="center">Science</td>
+	    				<td width="500px" align="center">2015</td>
+	    				<td width="500px" align="center">5.00</td>
+	    			</tr>
+	    			<tr>
+	    				<td height="50px" width="500px" align="center">HSC</td>
+	    				<td width="500px" align="center">Dhaka Imperial College</td>
+	    				<td width="500px" align="center">Science</td>
+	    				<td width="500px" align="center">2017</td>
+	    				<td width="500px" align="center">5.00</td>
+	    			</tr>
+                    <tr>
+	    				<td height="50px" width="500px" align="center">B.Sc in CSE</td>
+	    				<td width="500px" align="center">American International University Bangladesh</td>
+	    				<td width="500px" align="center">Science</td>
+	    				<td width="500px" align="center">2021</td>
+	    				<td width="500px" align="center">3.50</td>
+	    			</tr>
